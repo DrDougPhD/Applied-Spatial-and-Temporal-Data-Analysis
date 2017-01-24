a6cb729a48df62915a4e3218f68f678e2423fc30
Doug McGeehan <djmvfb@mst.edu>
2017-01-24 11:30:52 -0600

    attempting to get it working, but it seems to be difficult

--- cnn.py
+++ cnn.py
@@ -43,11 +43,28 @@ import newspaper
 
 logger = logging.getLogger(__appname__)
 
+"""
+def directory_in_cwd(directory, create=True):
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    directory_name = os.path.dirname(directory)
+    directory_abs_path = os.path.join(cwd, directory)
+    os.makedirs(directory_abs_path, exist_ok=create)
+    return directory_abs_path
+
+CACHE_DIR = directory_in_cwd('cache', create=False)
+"""
 
 def main(args):
+    newspaper_source = newspaper.build(args.newspaper_url)
+    text, url = retrieve(newspaper_source.articles, cache_in=args.cache_to)
+    if text:
+        text_lines = text.split('\n')
+        logger.debug(text_lines[0])
+        logger.debug('...')
+    else:
+        logger.error('No text downloaded from article: {}'.format(url))
+
     """
-    cnn_paper = newspaper.build('http://cnn.com')
-    archive(cnn_paper)
     for article in cnn_paper.articles:
         preprocess(article)
         articles.download()
@@ -60,12 +77,53 @@ def main(args):
     #preprocess(cnn_paper.articles[0])
 
 import hashlib
-def archive(newspaper):
-    for article in newspaper.articles:
-        md5 = hashlib.md5()
-        md5.update(article.url)
-        filename = h.hexdigest()
-        filepath = 'blah'
+def retrieve(articles, cache_in):
+    """
+    for article in articles:
+        yield retrieve_article(article, cache)
+    """
+    logger.debug('Retrieving articles, either from website or cache')
+    text = ''
+    url = ''
+    retrieved_article_count = 0
+    for article in articles:
+        text = retrieve_article(article, cache_in)
+        url = article.url
+        retrieved_article_count += 1
+        break
+    logger.debug('Retrieved {count} article{plural}'.format(
+            count=retrieved_article_count,
+            plural='s' if retrieved_article_count > 1 else ''))
+    return text, url
+
+
+def retrieve_article(article, cached_in=None):
+    logger.debug('Accessing {}'.format(article.url))
+    md5 = hashlib.md5()
+    md5.update(article.url.encode('utf-8'))
+    filename = '{hash}.txt'.format(h.hexdigest())
+    filepath = os.path.join(filename)
+
+    txt = ''
+    if os.path.isfile(filepath):
+        with open(filepath) as f:
+            txt = f.read()
+        logger.debug('Cached at {}'.format(filepath))
+        
+
+    else:
+        logger.debug('Cache not found. Downloading article')
+        article.download()
+        article.parse()
+        article.nlp()
+
+        with open(filepath, 'w') as f:
+            txt = article.text
+            f.write(txt)
+        logger.debug('Caching to {}'.format(filepath))
+
+    return txt
+
 
 def preprocess(article):
     article.download()
@@ -113,11 +171,15 @@ def get_arguments():
     # calling this with -v
     parser.add_argument('-v', '--verbose', action='store_true',
                         default=False, help='verbose output')
-    def directory_in_cwd(directory):
+
+    parser.add_argument('-n', '--newspaper', dest='newspaper_url',
+                        default='http://www.cnn.com/',
+                        help='URL for target newspaper')
+    def directory_in_cwd(directory, create=True):
         cwd = os.path.dirname(os.path.abspath(__file__))
         directory_name = os.path.dirname(directory)
         directory_abs_path = os.path.join(cwd, directory)
-        os.makedirs(directory_abs_path, exist_ok=True)
+        os.makedirs(directory_abs_path, exist_ok=create)
         return directory_abs_path
 
     parser.add_argument('-a', '--archive-to', dest='cache_to',
@@ -125,6 +187,7 @@ def get_arguments():
                         help='cache newspaper articles to directory')
     args = parser.parse_args()
     return args
+
 
 if __name__ == '__main__':
     try:
