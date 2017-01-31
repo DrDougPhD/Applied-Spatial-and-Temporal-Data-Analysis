debb27e49ae4368fa2b5079c61a28105ef301958
Doug McGeehan <djmvfb@mst.edu>
2017-01-30 22:39:41 -0600

    one column for each distance function

--- website.py
+++ website.py
@@ -2,6 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask import render_template
+from lib.interval import Interval
+
+ALERT_TYPE = {
+        Interval(0, 33): 'danger',
+        Interval(33, 66): 'warning',
+        Interval(66, 100): 'success',
+}
 
 @app.route('/')
 def index():
