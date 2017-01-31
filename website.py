8eb0fe7f9cc08c7ac20ecbb8cc920b2d0f3f655f
Doug McGeehan <djmvfb@mst.edu>
2017-01-30 19:41:30 -0600

    website now has bootstrap and a template

--- website.py
+++ website.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+from flask import render_template
 
-if __name__ == "__main__":
+@app.route('/')
+def index():
+    return render_template('index.html', similarities=[])
+
+if __name__ == '__main__':
     app.run()
