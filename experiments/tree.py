b94517be0cad4b89d2f6e301ef64826abad4b29b
Doug McGeehan <djmvfb@mst.edu>
2017-02-18 21:38:19 -0600

    forgot to add tree.py to the mix

---
+++ experiments/tree.py
@@ -0,0 +1,21 @@
+import logging
+logger = logging.getLogger('cnn.'+__name__)
+from sklearn import tree
+import numpy
+import processing
+
+def run(k, corpus):
+    accuracies = []
+    partitioner = processing.CrossValidation(k=k, dataset=corpus)
+    for training, testing in partitioner:
+        logger.info('Training Decision Tree')
+        clf = tree.DecisionTreeClassifier()
+        clf.fit(training.matrix, training.classes)
+
+        logger.info('Predicting scores')
+        accuracy = clf.score(testing.matrix, testing.classes)
+        accuracies.append(accuracy)
+    average_accuracy = numpy.mean(accuracies)
+
+    logger.info('Accuracy: {0} -- {1}'.format(average_accuracy, accuracies))
+    logger.debug('-'*120)
