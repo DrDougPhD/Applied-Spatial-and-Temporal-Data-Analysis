c73824e26dad541e8ce3c985a4178b84c366d236
Doug McGeehan <djmvfb@mst.edu>
2017-02-21 14:51:25 -0600

    preparing for building up the experiments for the decision tree

--- experiments/tree/experiment.py
+++ experiments/tree/experiment.py
@@ -1,8 +1,24 @@
 from experiments import LoggingObject
+from sklearn import tree
+import pydotplus
+import os
 
 class Experiment(LoggingObject):
     def __init__(self, cross_validation_n, corpus_series):
         super(Experiment, self).__init__()
+        self.n = cross_validation_n
+        self.datasets = corpus_series
 
-    def run(self, series, vector_type, splitting_criterion, x):
-        pass
+    def export(clf, feature_names, class_names, save_to, name):
+        dot_data = tree.export_graphviz(clf, out_file=None,
+                                        feature_names=feature_names,
+                                        class_names=class_names,
+                                        filled=True,
+                                        rounded=True,
+                                        special_characters=True)
+        graph = pydotplus.graph_from_dot_data(dot_data)
+        graph.write_pdf(os.path.join(save_to,
+                                     '{}.decision_tree.pdf'.format(name)))
+
+    def accuracy(self, series, vector_type, splitting_criterion, x):
+        return 4
---
+++ experiments/tree/plot.py
@@ -0,0 +1,6 @@
+
+def prec_n_rec(data):
+    pass
+
+if __name__ == '__main__':
+    pass
