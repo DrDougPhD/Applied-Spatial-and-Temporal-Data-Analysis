ca5b2ff149c7550f693509a0f085c911c0b00fab
Doug McGeehan <djmvfb@mst.edu>
2017-02-21 15:02:06 -0600

    making dummy subplots

--- experiments/tree/plot.py
+++ experiments/tree/plot.py
@@ -1,6 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from experiments import LoggingObject
+from lib.lineheaderpadded import hr
+import logging
+logger = logging.getLogger('cnn.'+__name__)
 
 def prec_n_rec(data):
-    pass
+    fig, ax = plt.subplots(nrows=2)
+
+    plt.show()
+
+class ExperimentResults(LoggingObject):
+    def __init__(self):
+        super(ExperimentResults, self).__init__()
+        self.accuracy = None
+        self.precision = None
+        self.recall = None
+        self.fmeasure = None
 
 if __name__ == '__main__':
-    pass
+    dummy_results = ExperimentResults()
+    prec_n_rec(dummy_results)
