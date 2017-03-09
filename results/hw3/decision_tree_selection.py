9f87e84aaae4eb13e6b2face22808c9c85c8877a
Doug McGeehan <djmvfb@mst.edu>
2017-03-09 07:54:47 -0600

    initial work towards decision tree selector, tying into scikit-learn's stuff

---
+++ results/hw3/decision_tree_selection.py
@@ -0,0 +1,66 @@
+import pprint
+import numpy as np
+import csv
+from progressbar import ProgressBar
+from sklearn.tree.tree import _splitter
+from sklearn.tree.tree import CRITERIA_CLF
+
+filename = 'corpus.tfidf.105.csv'
+
+
+def main():
+    matrix = []
+    labels = []
+    with open(filename) as f:
+        csv_file = csv.reader(f)
+        header = next(csv_file)[1:]
+        for row in csv_file:
+            labels.append(row[0])
+            matrix.append(np.array(row[1:], dtype=np.float_))
+    matrix = np.array(matrix).T
+    unique_labels = set(labels)
+    labels = np.array(labels, dtype=np.str_)
+    label_binaries = {
+        l: labels == l for l in unique_labels
+    }
+    print(matrix)
+
+    progress = ProgressBar(max_value=len(header)*len(unique_labels))
+    correlations = []
+    index = 0
+    splitter = _splitter.BestSparseSplitter(CRITERIA_CLF['gini'](len(labels),
+                                                                 np.array([1]*len(header))),
+                                            -1, 1,
+                                            0,
+                                            np.random.mtrand._rand,
+                                            False)
+
+    for i, feat_vector in enumerate(matrix):
+        for class_label in unique_labels:
+            corr = np.correlate(label_binaries[class_label],
+                                feat_vector)
+            correlations.append((class_label, header[i], corr[0]))
+
+            progress.update(index)
+            index += 1
+
+    progress.finish()
+
+    print('Sorting features by mutual information')
+    correlations.sort(key=lambda x: x[-1], reverse=True)
+    print('First 100 max relevant features:')
+    pprint.pprint(correlations[:100])
+
+    print('Writing out to file')
+    with open('class_to_features.tfidf.sorted.txt', 'w') as f:
+        progress = ProgressBar(max_value=len(correlations))
+        for i, corr in enumerate(correlations):
+            f.write('{0: >15} {1: <15}\t{2}\n'.format(*corr))
+            progress.update(i)
+
+        progress.finish()
+
+
+if __name__ == '__main__':
+    main()
+
