f6f705858e059a839047bd436c5f8f976b097829
Doug McGeehan <djmvfb@mst.edu>
2017-03-10 12:47:30 -0600

    feat selection from ranked tfidf scores

--- dimreduce/ranked_tfidf.py
+++ dimreduce/ranked_tfidf.py
@@ -1,12 +1,16 @@
 import csv
 import pprint
 import numpy
+import utils
+
+#@utils.pickled('n', 'filename')
+def select_by_ranked_tfidf(n, filename):
+    scores = ranked_tfidf(filename=filename)
+    return [feat for feat, num_occurrences, avg_score in scores[:n]]
 
 
-filename = 'corpus.tfidf.105.csv'
-
-
-def main():
+@utils.pickled('filename')
+def ranked_tfidf(filename):
     matrix = []
     labels = []
     with open(filename) as f:
@@ -43,6 +47,15 @@ def main():
         f.writelines(['{0: >20}\t{1: ^15}\t{2}\n'.format(*x)
                       for x in avg_feature_scores])
 
+    return avg_feature_scores
+
 
 if __name__ == '__main__':
-    main()
+    n = 100
+    ranked_scores = select_by_ranked_tfidf(n=n,
+                                           filename='corpus.tfidf.105.csv')
+    print('Highest tf-idf features:')
+    pprint.pprint(ranked_scores)
+    output = 'avgtfidf.tfidf.100articles.{n}terms.txt'.format(n=n)
+    with open(output, 'w') as f:
+        f.write('\n'.join(ranked_scores))
