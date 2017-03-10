9a84f6ef2154001e308d1711a05855ae6b390b11
Doug McGeehan <djmvfb@mst.edu>
2017-03-10 12:52:59 -0600

    random forest R script

---
+++ dimreduce/random_forest_selection.R
@@ -0,0 +1,16 @@
+library('randomForest')
+
+csv <- read.csv('corpus.tfidf.105.csv')
+frame <- data.frame(csv)
+
+
+classes <- frame[,1]
+matrix <- frame[,-1]
+
+print('Building random forest')
+forest <- randomForest(matrix, data=classes)
+scores <- importance(forest)
+
+print('----------------------------------------------------------')
+print('Random Forest writing out to file')
+write.csv(scores, file='randomforest.tfidf.105.csv')
