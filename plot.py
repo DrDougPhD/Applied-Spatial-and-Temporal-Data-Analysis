d6ebfe9f2c7e2210539221db615afccafe3684c8
Doug McGeehan <djmvfb@mst.edu>
2017-03-26 18:22:58 -0500

    got an idea that doesn't require transforming

--- plot.py
+++ plot.py
@@ -25,7 +25,9 @@ rmse_avg = [r['rmse-avg'] for r in lines]
 rmse_vals = [rmse_fold1, rmse_fold2, rmse_fold3, rmse_avg]
 
 labels = ['Fold 1', 'Fold 2', 'Fold 3', 'Mean']
+mae_access_order = ['rmse-fold1', 'rmse-fold2', 'rmse-fold3', 'rmse-avg']
 num_bars = len(mae_fold1)
+
 
 # create two subplot figure
 fig, (mae_axes, rmse_axes) = plt.subplots(2, sharex=True)
@@ -35,6 +37,10 @@ rmse_axes.set_ylabel('RMSE')
 
 # draw subplot for mae
 bar_slots_to_occupy = num_bars + 1
+base_indices = np.arange(start=1,
+                         stop=bar_slots_to_occupy*len(labels)+1,
+                         step=bar_slots_to_occupy)
+print(base_indices)
 for bar_offset, result_group in enumerate(mae_vals):
     # draw bars for fold k / average results
     indices = np.arange(num_bars)+1+(bar_offset*bar_slots_to_occupy)
