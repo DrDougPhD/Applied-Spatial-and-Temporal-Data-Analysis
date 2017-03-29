9c79ccc89bbf2a10ebf330709135ca4d111aec2c
Doug McGeehan <djmvfb@mst.edu>
2017-03-28 23:10:20 -0500

    transforming records for plotting

---
+++ bestk.py
@@ -0,0 +1,92 @@
+from surprise import Dataset
+from surprise import Reader
+from surprise import evaluate
+from surprise import print_perf
+import os
+import pprint
+
+from surprise import KNNBasic # collaborative filtering
+
+import threading
+import time
+import statistics
+import json
+import collections
+
+exitFlag = 0
+
+PERF_MEASURES = ['RMSE', 'MAE']
+
+#load data from a file
+FILE_PATH = os.path.join('data', 'restaurant_ratings.txt')
+FILE_READER = Reader(line_format='user item rating timestamp', sep='\t')
+
+
+def main():
+    data = Dataset.load_from_file(FILE_PATH,
+                                  reader=FILE_READER)
+    data.split(n_folds=3)
+
+    methods = [ucf, icf]
+    jobs = []
+    for i, method in enumerate(methods):
+        for k in range(2, 101):
+            job = ParallelRecommenders(threadID=i,
+                                       counter=1,
+                                       fn=method,
+                                       data=data,
+                                       k=k)
+            job.start()
+            jobs.append(job)
+        
+    
+    performances = collections.defaultdict(list)
+    for j in jobs:
+        j.join()
+        performances[j.name].append(j.perf)
+    
+    print('='*80)
+    pprint.pprint(performances)
+    
+    print('Writing to file')
+    with open('kvaried.json', 'w') as f:
+        json.dump(performances, f)
+
+
+class ParallelRecommenders(threading.Thread):
+   def __init__(self, threadID, counter, fn, data, k):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = fn.__doc__
+      self.counter = counter
+      self.method = fn
+      self.params = data
+      self.k = k
+      
+   def run(self):
+      print("Starting {0} (k={1}".format(self.name, self.k))
+      self.perf = (self.k, self.method(self.params, self.k))
+      print("Exiting  {0} (k={1}".format(self.name, self.k))
+
+def _collaborative_filtering(data, is_user_based, kneighbors):
+    algo = KNNBasic(k=kneighbors,
+                    sim_options = {
+        'user_based': is_user_based,
+        'name': 'MSD',
+    })
+    return evaluate(algo, data,
+                    measures=PERF_MEASURES)
+
+
+def ucf(data, k):
+    """User-based Collaborative Filtering"""
+    return _collaborative_filtering(data, True, k)
+
+
+def icf(data, k):
+    """Item-based Collaborative Filtering"""
+    return _collaborative_filtering(data, False, k)
+
+
+if __name__ == '__main__':
+    main()
---
+++ variable_k_plot.py
@@ -0,0 +1,95 @@
+import json
+import pprint
+import matplotlib.pyplot as plt
+import numpy as np
+import collections
+
+HATCHES = ('/', '', '*', 'o', 'O', '.')
+
+
+# load data
+lines = []
+with open('kvaried.json') as f:
+    results = json.load(f)
+
+
+# transform to be friendly for plotting
+indices = None
+mae_averages = collections.defaultdict(list)
+rmse_averages = collections.defaultdict(list)
+for method, values in results.items():
+    if indices is None:
+        indices = [k for k, _ in values]
+
+    mae_averages[method] = [np.mean(values_for_k['mae'])
+                            for k, values_for_k in values]
+    rmse_averages[method] = [np.mean(values_for_k['rmse'])
+                             for k, values_for_k in values]
+
+pprint.pprint(mae_averages)
+pprint.pprint(rmse_averages)
+
+"""
+mae_results = [
+    [np.mean(results[method]['mae'])
+     for sim in results[method]]
+    for method in results
+]
+rmse_results = [
+    [np.mean(results[method]['rmse'])
+     for  in results[method]]
+    for method in results
+]
+access_order = list(results.keys())
+
+sample_method = list(results.keys())[0]
+sim_order = list(results[sample_method].keys())
+
+
+# define the order for plotting based on descending average
+num_bars_per_method = 3
+num_bars_per_group = 2
+
+
+# create two subplot figure
+fig, (mae_axes, rmse_axes) = plt.subplots(2, sharex=True)
+mae_axes.set_ylabel('MAE (avg)')
+rmse_axes.set_ylabel('RMSE (avg)')
+
+
+# draw subplot for mae
+bar_slots_to_occupy = num_bars_per_group + 1
+base_indices = np.arange(start=1,
+                         stop=bar_slots_to_occupy*num_bars_per_method+1,
+                         step=bar_slots_to_occupy)
+for i, method in enumerate(access_order):
+    # draw bars for fold k / average results
+    indices = base_indices + i
+
+    rmse_axes.bar(indices, rmse_results[i], label=method, hatch=2*HATCHES[i])
+
+    mae_axes.bar(indices, mae_results[i], label=method, hatch=2*HATCHES[i])
+
+
+# apply legend
+plt.sca(rmse_axes)
+plt.legend(bbox_to_anchor=(0., -1.05, 1., .102), loc=8,
+           borderaxespad=1.)
+plt.xticks(base_indices+0.5, sim_order)
+plt.subplots_adjust(bottom=0.35)
+plt.savefig('sim_comp.svg')
+
+# create a table of averages
+with open('sim_averages.txt', 'w') as f:
+    f.write('{0: >35} & {1: >10} & {2} & {3} \\\\ \n'
+            '\hline \\\\ \n'.format(
+        'Method', 'Measurement', 'RMSE (Mean)', 'MAE (Mean)'))
+
+    for method, values_by_metric in results.items():
+        for metric, values in values_by_metric.items():
+            f.write('{0: >35} &  {1: >11} & {2:.4f}   &   {3:.4f}    \\\\ \n'.format(
+                    method, metric,
+                    np.mean(values['rmse']),
+                    np.mean(values['mae'])))
+"""
+
